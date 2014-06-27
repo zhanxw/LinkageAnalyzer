c271472c9c1985c8b27c90b63c3479fc7f0f49a2
zhanxw <zhanxw@gmail.com>
2014-06-27 11:51:06 -0500

    v1.0.7

--- LinkageAnalysis/R/double_link.R
+++ LinkageAnalysis/R/double_link.R
@@ -34,6 +34,8 @@ double_link <- function(main_file, G2_file = "", output = ".", test = "woG2",
           if (ret$returncode == 1 && ret$message == "dichotomize failed") {
             # this is a special error,
             # meaning we will treat it as normal exit but no output files
+            # so returncode are changed from 1 to 0
+            ret$returncode = 0
             msg <- paste("Exit successfully with no outputs due to", ret$message)
           } else {
             msg <- paste("Exit failed", ret$message, sep = " ")
